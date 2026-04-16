4961e4fc59190168f065178cf1ee725214aa5fde
pethromuse <pethromuse@gmail.com>
2026-04-16 09:37:10 +0900

    init project

---
+++ src/main/java/com/swingpop/lindyhopseoul/LindyhopseoulApplication.java
@@ -0,0 +1,13 @@
+package com.swingpop.lindyhopseoul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LindyhopseoulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LindyhopseoulApplication.class, args);
+    }
+
+}
